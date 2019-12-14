4653f035233ccabd9d2164dc930f6f1a757e0b3c
juan <jdgn94@gmail.com>
2019-12-14 08:07:03 -0400

    imagenes de avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar page"),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://elpais.com/elpais/imagenes/2018/11/13/gente/1542123826_040867_1542176366_noticia_fotograma.jpg'),
+              radius: 23.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 5.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.purple,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://wallpapercave.com/wp/wp3188330.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
