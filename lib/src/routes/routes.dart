02eefe5e97a9ba96e793a67f032921fa09e69630
juan <jdgn94@gmail.com>
2019-12-14 11:45:57 -0400

    Creacion de animacion con su respectiva ruta

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,12 +4,14 @@ import 'package:componets/src/pages/alert_page.dart';
 import 'package:componets/src/pages/avatar_page.dart';
 import 'package:componets/src/pages/home_page.dart';
 import 'package:componets/src/pages/card_page.dart';
+import 'package:componets/src/pages/animated_container_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
     'home': (BuildContext context) => HomePage(),
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
-    'card': (BuildContext context) => CardPage()
+    'card': (BuildContext context) => CardPage(),
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
   };
 }
